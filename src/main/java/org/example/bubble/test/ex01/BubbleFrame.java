d5350375e8d69ae334e92b4a0ff650dd17a9c66b
JaeHeon Lee <romeo92s@naver.com>
2022-09-29 23:58:44 +0900

    환경설정 완료

---
+++ src/main/java/org/example/bubble/test/ex01/BubbleFrame.java
@@ -0,0 +1,16 @@
+package org.example.bubble.test.ex01;
+
+import javax.swing.*;
+
+public class BubbleFrame extends JFrame {
+    public BubbleFrame() {
+        setSize(1000, 640);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new BubbleFrame();
+    }
+
+}
